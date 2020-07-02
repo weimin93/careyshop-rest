ed8c4638e96f990a9a0bf9aaf304f206dfed4dc7
思维空间 <dnyz520@126.com>
2020-07-02 15:52:12 +0800

    no message

--- src/store/modules/careyshop/modules/db.js
+++ src/store/modules/careyshop/modules/db.js
@@ -1,5 +1,3 @@
-import router from '@/router'
-import { cloneDeep } from 'lodash'
 import { database as getDatabase, dbGet, dbSet } from '@/utils/util.db'
 
 export default {
@@ -12,15 +10,13 @@ export default {
      * @param {Object} payload dbName {String} 数据库名称
      * @param {Object} payload path {String} 存储路径
      * @param {Object} payload value {*} 需要存储的值
-     * @param {Object} payload user {Boolean} 是否区分用户
      */
     set(context, {
       dbName = 'database',
       path = '',
-      value = '',
-      user = false
+      value = ''
     }) {
-      dbSet({ dbName, path, value, user })
+      dbSet({ dbName, path, value })
     },
     /**
      * @description 获取数据
@@ -29,126 +25,31 @@ export default {
      * @param {Object} payload dbName {String} 数据库名称
      * @param {Object} payload path {String} 存储路径
      * @param {Object} payload defaultValue {*} 取值失败的默认值
-     * @param {Object} payload user {Boolean} 是否区分用户
      */
     get(context, {
       dbName = 'database',
       path = '',
-      defaultValue = '',
-      user = false
+      defaultValue = ''
     }) {
-      return dbGet({ dbName, path, defaultValue, user })
+      return dbGet({ dbName, path, defaultValue })
     },
     /**
      * @description 获取存储数据库对象
      * @param {Object} context
-     * @param {Object} payload user {Boolean} 是否区分用户
      */
     database(context, {
-      user = false
     } = {}) {
       return getDatabase({
-        user,
         defaultValue: {}
       })
     },
     /**
      * @description 清空存储数据库对象
      * @param {Object} context
-     * @param {Object} payload user {Boolean} 是否区分用户
      */
     databaseClear(context, {
-      user = false
     } = {}) {
       return getDatabase({
-        user,
-        validator: () => false,
-        defaultValue: {}
-      })
-    },
-    /**
-     * @description 获取存储数据库对象 [ 区分页面 ]
-     * @param {Object} context
-     * @param {Object} payload basis {String} 页面区分依据 [ name | path | fullPath ]
-     * @param {Object} payload user {Boolean} 是否区分用户
-     */
-    databasePage(context, {
-      basis = 'fullPath',
-      user = false
-    } = {}) {
-      return getDatabase({
-        path: `$page.${router.app.$route[basis]}`,
-        user,
-        defaultValue: {}
-      })
-    },
-    /**
-     * @description 清空存储数据库对象 [ 区分页面 ]
-     * @param {Object} context
-     * @param {Object} payload basis {String} 页面区分依据 [ name | path | fullPath ]
-     * @param {Object} payload user {Boolean} 是否区分用户
-     */
-    databasePageClear(context, {
-      basis = 'fullPath',
-      user = false
-    } = {}) {
-      return getDatabase({
-        path: `$page.${router.app.$route[basis]}`,
-        user,
-        validator: () => false,
-        defaultValue: {}
-      })
-    },
-    /**
-     * @description 快速将页面当前的数据 ( $data ) 持久化
-     * @param {Object} context
-     * @param {Object} payload instance {Object} vue
-     * @param {Object} payload basis {String} 页面区分依据 [ name | path | fullPath ]
-     * @param {Object} payload user {Boolean} 是否区分用户
-     */
-    pageSet(context, {
-      instance,
-      basis = 'fullPath',
-      user = false
-    }) {
-      return getDatabase({
-        path: `$page.${router.app.$route[basis]}.$data`,
-        user,
-        validator: () => false,
-        defaultValue: cloneDeep(instance.$data)
-      })
-    },
-    /**
-     * @description 快速获取页面快速持久化的数据
-     * @param {Object} context
-     * @param {Object} payload instance {Object} vue
-     * @param {Object} payload basis {String} 页面区分依据 [ name | path | fullPath ]
-     * @param {Object} payload user {Boolean} 是否区分用户
-     */
-    pageGet(context, {
-      instance,
-      basis = 'fullPath',
-      user = false
-    }) {
-      return dbGet({
-        path: `$page.${router.app.$route[basis]}.$data`,
-        user,
-        defaultValue: cloneDeep(instance.$data)
-      })
-    },
-    /**
-     * @description 清空页面快照
-     * @param {Object} context
-     * @param {Object} payload basis {String} 页面区分依据 [ name | path | fullPath ]
-     * @param {Object} payload user {Boolean} 是否区分用户
-     */
-    pageClear(context, {
-      basis = 'fullPath',
-      user = false
-    }) {
-      return getDatabase({
-        path: `$page.${router.app.$route[basis]}.$data`,
-        user,
         validator: () => false,
         defaultValue: {}
       })
