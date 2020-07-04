8ed229dcfd77f4cfb2fbbf75d6cd3a34ebce9211
思维空间 <dnyz520@126.com>
2020-07-05 01:16:56 +0800

    no message

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,30 +1,30 @@
 <template>
   <div>
-    <cs-card title="请求">
+    <cs-card :title="$t('request')">
       <el-form ref="request" :model="request" label-width="90px">
-        <el-form-item label="地址">
+        <el-form-item :label="$t('url')">
           <el-input v-model="request.url"></el-input>
         </el-form-item>
 
-        <el-form-item label="参数">
-          <el-input v-model="request.url"></el-input>
+        <el-form-item :label="$t('payload')">
+          <el-input v-model="request.payload"></el-input>
         </el-form-item>
       </el-form>
     </cs-card>
 
-    <cs-card title="请求头" :expanded="true">
+    <cs-card :title="$t('headers')" :expanded="true">
       <div>this is headers</div>
     </cs-card>
 
-    <cs-card title="登陆" :expanded="true">
-      <div>this is authentication</div>
+    <cs-card :title="$t('login')" :expanded="true">
+      <div>this is log in</div>
     </cs-card>
 
     <div class="cs-mb cs-tr">
-      <el-button type="primary">发送请求</el-button>
+      <el-button type="primary">{{$t('send request')}}</el-button>
     </div>
 
-    <cs-card title="响应">
+    <cs-card :title="$t('response')">
       <div>this is response</div>
     </cs-card>
   </div>
