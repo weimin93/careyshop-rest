b277fb03f97069ebb282042a46dc8816fe9c10cb
思维空间 <dnyz520@126.com>
2020-07-09 06:39:19 +0800

    up

--- src/views/components/mixins/sendRequest.js
+++ src/views/components/mixins/sendRequest.js
@@ -1,24 +1,27 @@
 import util from '@/utils/util'
-import axios from "axios";
+import axios from 'axios'
 
 export default {
   methods: {
+    _replace(value) {
+      return util.settingReplace(value, this.setting.variable)
+    },
     cancel() {
       // this.sendEnd = true
       // this.sendLoading = false
     },
     async submit() {
       // 解析请求地址
-      let url = util.settingReplace(this.request.url, this.setting.variable)
+      let url = this._replace(this.request.url)
 
       // 解析请求参数
-      let payload = util.settingReplace(this.request.payload, this.setting.variable)
+      let payload = this._replace(this.request.payload)
 
       // 解析请求头
       let headers = {}
       this.headers.forEach(val => {
-        const name = util.settingReplace(val.name, this.setting.variable)
-        headers[name] = util.settingReplace(val.value, this.setting.variable)
+        const name = this._replace(val.name)
+        headers[name] = this._replace(val.value)
       })
 
       // 初始化各类数据
@@ -34,11 +37,15 @@ export default {
         baseURL: url,
         method: this.request.method,
         timeout: 30000,
-        headers: headers,
-        withCredentials: true
+        headers: headers
       })
 
-      console.dir(service)
+      service({
+        params: { method: 'get.article.list' }
+      })
+        .then(res => {
+          console.log(res)
+        })
     }
   }
 }
