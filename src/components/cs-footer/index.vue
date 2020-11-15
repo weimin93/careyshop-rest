661559261a828b1a67dcc09a1532264572e8af1f
思维空间 <dnyz520@126.com>
2020-11-16 00:44:43 +0800

    package update

--- src/components/cs-footer/index.vue
+++ src/components/cs-footer/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="cs-footer cs-tc">
-    <span>Copyright &copy; 2017-2020 温州瑞令网络科技有限公司 All rights reserved.</span>
+    <span>Copyright &copy; 2017-2020 宁波互明科技有限公司 All rights reserved.</span>
     <p><a href="https://www.careyshop.cn" target="_blank">www.careyshop.cn</a></p>
   </div>
 </template>
